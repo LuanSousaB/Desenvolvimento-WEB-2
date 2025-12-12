0fc156685497567cf7f5d67d47edecce3a0ead69
LuanSousaB <luan.sousab2004@gmail.com>
2025-12-12 20:16:36 -0300

    Update 4.OrientObjeto.js

--- Unidade_1/4.OrientObjeto.js
+++ Unidade_1/4.OrientObjeto.js
@@ -14,14 +14,15 @@ class Livro {
 
 console.log("--- Instâncias da Classe Livro ---");
 
-// a) Criar duas instâncias
+
 const livroA = new Livro("A Batalha do Apocalipse", "Eduardo Spohr", 2007);
 const livroB = new Livro("Dom Casmurro", "Machado de Assis", 1899);
 
 console.log("\nPrimeiro Livro:");
-// b) Chamar o método detalhes() para o livro A
+
 livroA.detalhes();
 
 console.log("\nSegundo Livro:");
-// b) Chamar o método detalhes() para o livro B
+
 livroB.detalhes();
+
